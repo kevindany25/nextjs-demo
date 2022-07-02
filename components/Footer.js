b7f7cf4cbb775106278652b177781207563aae2e
Kevin <kevin.dany25@gmail.com>
2022-07-02 18:52:36 -0500

    correccion4

--- components/Footer.js
+++ components/Footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
         <h1 className={styles.linkTitle}>
           <Link href="/contact">
             <span className={styles.linkText}>CONTACTANOS</span>
-            <Image src="/img/link.png" width="40px" height="40px" alt=""/>
           </Link>
+          <Image src="/img/link.png" width="40px" height="40px" alt=""/>
         </h1>
       </div>
       <div className={styles.cardS}>
